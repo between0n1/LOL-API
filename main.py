c6e3e6d3589bde12a0d5699e004320f35f7e44af
between0n1 <67476090+between0n1@users.noreply.github.com>
2021-01-12 20:54:35 +0900

    number of players by division, tier, region
    edited some codes to make it faster.

--- main.py
+++ main.py
@@ -3,43 +3,45 @@
 
 import requests
 import time
-import csv
 import pandas as pd
 from datetime import date
 
 
-def get_numberofplayers_by_division(region,queue, tier, division):
+#This gives you the number of players in a specific division (GOLD IV) in a region
+def players(region, queue, tier, division):
     accumulated_players_by_division = 0
-    page_num = 1
-    isend = False
-    while isend == False:
+    for page_num in range(0,50000):
         URL = "https://"+region+".api.riotgames.com/lol/league/v4/entries/" + queue + "/" + tier + "/" + division + "?page=" + \
               str(page_num)
         res = requests.get(URL, headers={"X-Riot-Token": api_key})
-        accumulated_players_by_division += len(res.json())
-        page_num += 1
-        if len(res.json()) == 0:
-            isend = True
-        if res.status_code == 429:
+        while res.status_code == 429:  # rate limit for personal keys is 100 requests every 2 minutes.
+            print("wating........")
             time.sleep(10)
-            page_num -= 1
+            URL = "https://" + region + ".api.riotgames.com/lol/league/v4/entries/" + queue + "/" + tier + "/" + division + "?page=" + \
+                  str(page_num)
+            res = requests.get(URL, headers={"X-Riot-Token": api_key})
+        if len(res.json()) == 0:
+            break
+        accumulated_players_by_division += len(res.json())
     return(accumulated_players_by_division)
 
-def total_number(region):
+def total_players(region):
     queue = "RANKED_SOLO_5x5"
     tier_list = ["DIAMOND", "PLATINUM", "GOLD", "SILVER", "BRONZE", "IRON"]
     division_list = ["I", "II", "III", "IV"]
     for tier in tier_list:
         for division in division_list:
-            asd = get_numberofplayers_by_division(region, queue, tier, division)
+            asd = players(region, queue, tier, division)
             print("Completed the calculation of " + tier + " " + division + ":" + str(asd))
             df = pd.DataFrame([[tier + " " + division, asd]])
             df.to_csv(region + "result" + str(today.strftime("%m%d%y")) + ".csv", mode='a', header=False)
 
+
+# Calculates the average tier of a region.
 def average_tier(filename):
     result = 0
     agg_num = 0
-    header_list = ["Index","Tiers","Players"]
+    header_list = ["Index", "Tiers", "Players"]
     data = pd.read_csv(filename, names=header_list)
     cus_mmr = pd.read_csv("DIA1~IRN4customized_mmr.csv", names = ['MMR'])
     for i in range(24):
@@ -48,17 +50,12 @@ def average_tier(filename):
     avg_tier = result / agg_num
     return avg_tier
 
-api_key = "RGAPI-6650c092-f632-4f41-9e15-136268b20ccb" # <----- put your api key
 queue = "RANKED_SOLO_5x5"
 tier_list = ["DIAMOND", "PLATINUM", "GOLD", "SILVER", "BRONZE", "IRON"]
 division_list = ["I","II","III","IV"]
-region = "na1" # <--------- edit this to attain data from another region ex: kr
+region = "kr" # <--------- edit this to attain data from another region ex: kr
 today = date.today()
-total_number(region) # <---- make a csv file that contains the number of players in each divisions (ex GOLD I or GOLD II) in the region
-asd = average_tier("na1result010421.csv") # <------------ the average_tier of the region
-
-
-
+api_key = None
 
 
 
